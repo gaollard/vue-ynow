4a8a9ad09284e8868de85404231d61f2569d44b2
gaollard <gaollard@foxmail.com>
2019-02-21 21:58:02 +0800

    update

---
+++ src/utils/index.ts
@@ -0,0 +1,41 @@
+/* eslint-disable no-empty */
+/* eslint-disable getter-return */
+/* eslint-disable import/no-mutable-exports */
+// import { isServer } from '.';
+
+export const isServer = false
+export let supportsPassive = false;
+
+if (!isServer) {
+  try {
+    const opts = {};
+    Object.defineProperty(opts, 'passive', {
+      get() {
+        /* istanbul ignore next */
+        supportsPassive = true;
+      }
+    });
+    window.addEventListener('test-passive', null, opts);
+  } catch (e) {}
+}
+
+export function on(target, event, handler, passive = false) {
+  !isServer &&
+    target.addEventListener(
+      event,
+      handler,
+      supportsPassive ? { capture: false, passive } : false
+    );
+}
+
+export function off(target, event, handler) {
+  !isServer && target.removeEventListener(event, handler);
+}
+
+export function stop(event) {
+  event.stopPropagation();
+}
+
+export function prevent(event) {
+  event.preventDefault();
+}
--- src/views/welcome/index.vue
+++ src/views/welcome/index.vue
@@ -1,14 +1,35 @@
 <template>
   <div class="view-welcome">
-    welcome
+    <Swipe class="swiper">
+      <SwipeItem class="swiper-slide">1</SwipeItem>
+      <SwipeItem class="swiper-slide">2</SwipeItem>
+    </Swipe>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 import Component from 'vue-class-component'
+import { Swipe, SwipeItem } from 'vant'
 
-@Component
+@Component({
+  components: {
+    Swipe,
+    SwipeItem
+  }
+})
 export default class WelcomeView extends Vue {
 }
 </script>
+
+<style lang="scss" scoped>
+.swiper {
+  height: 180px;
+}
+.swiper-slide:nth-child(1) {
+  background: #03a9f4;
+}
+.swiper-slide:nth-child(2) {
+  background: #009688;
+}
+</style>
