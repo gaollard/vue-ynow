d309d13d268b39a58972476d7ef63c500177b2a7
Arraybuffer <gaoxiong@huishoubao.com.cn>
2019-02-28 16:16:31 +0800

    测试 8001 端口 被限制的问题

--- src/config.ts
+++ src/config.ts
@@ -6,4 +6,4 @@ export const host = process.env.NODE_ENV !== 'development'
 //   ? 'ws://ynowapi.airtlab.com'
 //   : 'ws://127.0.0.1:8001'
 
-export const wsHost = 'ws://39.108.138.159:8001'
+export const wsHost = 'ws://ynowapi.airtlab.com:8001'
