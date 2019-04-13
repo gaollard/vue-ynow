3bed4b011ca6b66b02f633fd9987c623a451a0f6
gaollard <gaollard@foxmail.com>
2019-04-13 23:04:07 +0800

    update

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import vueg from 'vueg'
 
 import './style/base.scss'
 import './style/cover.scss'
 import 'vant/lib/icon/local.css'
-
-Vue.use(vueg, router, {
-})
 
 import {
   Cell,
